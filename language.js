9be0bb649bd389db51f4fdcbad2216b4ae43a696
Luiz Gustavo dos Santos Ribeiro <luiz.sribeiro@totvs.com.br>
2024-11-09 12:25:44 -0300

    CURRICULO_INICIO

--- language.js
+++ language.js
@@ -2,29 +2,30 @@ let translations = {};
 
 // Função para carregar as traduções
 async function loadTranslations(lang) {
-  const response = await fetch(`${lang}.json`);
-  translations = await response.json();
-  updateTexts();
+	const response = await fetch(`${lang}.json`);
+	translations = await response.json();
+	updateTexts();
 }
 
 // Função para atualizar o conteúdo dos elementos
 function updateTexts() {
-  document.getElementById('home').innerText = translations.home;
-  document.getElementById('curriculum').innerText = translations.curriculum;
-  document.getElementById('projects').innerText = translations.projects;
-  document.getElementById('contact').innerText = translations.contact;
-  document.getElementById('footer').innerText = translations.footer;
-  document.getElementById('profile_text').innerText = translations.profile_text;
-  document.getElementById('profile_text_v2').innerText =
-    translations.profile_text_v2;
-  document.getElementById('title_proj_1').innerText = translations.title_proj_1;
-  document.getElementById('text_proj_1').innerText = translations.text_proj_1;
-  document.getElementById('title_proj_2').innerText = translations.title_proj_2;
-  document.getElementById('text_proj_2').innerText = translations.text_proj_2;
-  document.getElementById('title_proj_3').innerText = translations.title_proj_3;
-  document.getElementById('text_proj_3').innerText = translations.text_proj_3;
-  document.getElementById('title_proj_4').innerText = translations.title_proj_3;
-  document.getElementById('text_proj_4').innerText = translations.text_proj_3;
+	document.getElementById('home').innerHTML = translations.home;
+	document.getElementById('curriculum').innerHTML = translations.curriculum;
+	document.getElementById('projects').innerHTML = translations.projects;
+	document.getElementById('contact').innerHTML = translations.contact;
+	document.getElementById('footer').innerHTML = translations.footer;
+	document.getElementById('profile_text').innerHTML = translations.profile_text;
+	document.getElementById('title_proj_1').innerHTML = translations.title_proj_1;
+	document.getElementById('text_proj_1').innerHTML = translations.text_proj_1;
+	document.getElementById('title_proj_2').innerHTML = translations.title_proj_2;
+	document.getElementById('text_proj_2').innerHTML = translations.text_proj_2;
+	document.getElementById('title_proj_3').innerHTML = translations.title_proj_3;
+	document.getElementById('text_proj_3').innerHTML = translations.text_proj_3;
+	document.getElementById('title_proj_4').innerHTML = translations.title_proj_3;
+	document.getElementById('text_proj_4').innerHTML = translations.text_proj_3;
+	document.getElementById('curriculo_title').innerHTML = translations.curriculo_title;
+	document.getElementById('section_header').innerHTML = translations.section_header;
+	document.getElementById('section_resume').innerHTML = translations.section_resume;
 }
 
 // Carrega o idioma inicial
@@ -32,5 +33,5 @@ loadTranslations('pt');
 
 // Função para trocar o idioma
 function changeLanguage(lang) {
-  loadTranslations(lang);
+	loadTranslations(lang);
 }
