c2d2003f7b67e480f3debc68b16bd7502f7dc545
Luiz Gustavo dos Santos Ribeiro <luiz.sribeiro@totvs.com.br>
2024-11-10 19:49:48 -0300

    ..

--- language.js
+++ language.js
@@ -2,68 +2,78 @@ let translations = {};
 
 // Função para carregar as traduções
 async function loadTranslations(lang) {
-  const response = await fetch(`${lang}.json`);
-  translations = await response.json();
-  updateTexts();
+	const response = await fetch(`${lang}.json`);
+	translations = await response.json();
+	updateTexts();
 }
 
 // Função para atualizar o conteúdo dos elementos
 function updateTexts() {
-  const elementsToTranslate = {
-    home: 'home',
-    curriculum: 'curriculum',
-    projects: 'projects',
-    contact: 'contact',
-    footer: 'footer',
-    profile_text: 'profile_text',
-    title_proj_1: 'title_proj_1',
-    text_proj_1: 'text_proj_1',
-    title_proj_2: 'title_proj_2',
-    text_proj_2: 'text_proj_2',
-    title_proj_3: 'title_proj_3',
-    text_proj_3: 'text_proj_3',
-    title_proj_4: 'title_proj_4',
-    text_proj_4: 'text_proj_4',
-    curriculo_title: 'curriculo_title',
-    section_header: 'section_header',
-    section_resume: 'section_resume',
-    title_educacao: 'title_educacao',
-    title_carreira: 'title_carreira',
-    cotemig: 'cotemig',
-    faculdade_cotemig: 'faculdade_cotemig',
-    unibh: 'unibh',
-    faculdade_cotemig_ads: 'faculdade_cotemig_ads',
-    xp_educacao: 'xp_educacao',
-    construsite: 'construsite',
-    nix_sistemas: 'nix_sistemas',
-    buscar_id: 'buscar_id',
-    argument: 'argument',
-    dwebnet: 'dwebnet',
-    totvs: 'totvs',
-    titulo_projeto_1: 'titulo_projeto_1',
-    descricao_projeto_1: 'descricao_projeto_1',
-    titulo_projeto_2: 'titulo_projeto_2',
-    descricao_projeto_2: 'descricao_projeto_2',
-    titulo_projeto_3: 'titulo_projeto_3',
-    descricao_projeto_3: 'descricao_projeto_3',
-    titulo_projeto_4: 'titulo_projeto_4',
-    descricao_projeto_4: 'descricao_projeto_4',
-    titulo_projeto_5: 'titulo_projeto_5',
-    descricao_projeto_5: 'descricao_projeto_5',
-  };
+	const elementsToTranslate = {
+		home: 'home',
+		curriculum: 'curriculum',
+		projects: 'projects',
+		contact: 'contact',
+		footer: 'footer',
+		profile_text: 'profile_text',
+		title_proj_1: 'title_proj_1',
+		text_proj_1: 'text_proj_1',
+		title_proj_2: 'title_proj_2',
+		text_proj_2: 'text_proj_2',
+		title_proj_3: 'title_proj_3',
+		text_proj_3: 'text_proj_3',
+		title_proj_4: 'title_proj_4',
+		text_proj_4: 'text_proj_4',
+		curriculo_title: 'curriculo_title',
+		section_header: 'section_header',
+		section_resume: 'section_resume',
+		title_educacao: 'title_educacao',
+		title_carreira: 'title_carreira',
+		cotemig: 'cotemig',
+		faculdade_cotemig: 'faculdade_cotemig',
+		unibh: 'unibh',
+		faculdade_cotemig_ads: 'faculdade_cotemig_ads',
+		xp_educacao: 'xp_educacao',
+		construsite: 'construsite',
+		nix_sistemas: 'nix_sistemas',
+		buscar_id: 'buscar_id',
+		argument: 'argument',
+		dwebnet: 'dwebnet',
+		totvs: 'totvs',
+		titulo_projeto_1: 'titulo_projeto_1',
+		descricao_projeto_1: 'descricao_projeto_1',
+		titulo_projeto_2: 'titulo_projeto_2',
+		descricao_projeto_2: 'descricao_projeto_2',
+		titulo_projeto_3: 'titulo_projeto_3',
+		descricao_projeto_3: 'descricao_projeto_3',
+		titulo_projeto_4: 'titulo_projeto_4',
+		descricao_projeto_4: 'descricao_projeto_4',
+		titulo_projeto_5: 'titulo_projeto_5',
+		descricao_projeto_5: 'descricao_projeto_5',
+		// Campos do formulário de contato
+		campo_nome: 'campo_nome',
+		campo_email: 'campo_email',
+		campo_telefone: 'campo_telefone',
+		campo_mensagem: 'campo_mensagem',
+	};
 
-  Object.keys(elementsToTranslate).forEach((id) => {
-    const element = document.getElementById(id);
-    if (element && translations[id]) {
-      element.innerHTML = translations[id];
-    }
+	Object.keys(elementsToTranslate).forEach((id) => {
+		const element = document.getElementById(id);
+		if (element && translations[id]) {
+			// Atualiza o innerHTML para textos normais e o placeholder para campos de entrada
+			if (element.tagName === 'INPUT' || element.tagName === 'TEXTAREA') {
+				element.placeholder = translations[id];
+			} else {
+				element.innerHTML = translations[id];
+			}
+		}
 
-    // Check if there is a description translation for tooltips
-    const descriptionKey = `${id}_description`;
-    if (element && translations[descriptionKey]) {
-      element.setAttribute('data-description', translations[descriptionKey]);
-    }
-  });
+		// Check if there is a description translation for tooltips
+		const descriptionKey = `${id}_description`;
+		if (element && translations[descriptionKey]) {
+			element.setAttribute('data-description', translations[descriptionKey]);
+		}
+	});
 }
 
 // Carrega o idioma inicial
@@ -71,5 +81,5 @@ loadTranslations('pt');
 
 // Função para trocar o idioma
 function changeLanguage(lang) {
-  loadTranslations(lang);
+	loadTranslations(lang);
 }
--- script.js
+++ script.js
@@ -1,121 +1,156 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const canvas = document.createElement('canvas');
-  const ctx = canvas.getContext('2d');
-  const network = document.getElementById('network');
-  network.appendChild(canvas);
+	const canvas = document.createElement('canvas');
+	const ctx = canvas.getContext('2d');
+	const network = document.getElementById('network');
+	network.appendChild(canvas);
 
-  let width, height;
-  const particles = [];
-  const maxParticles = 350;
-  const mouseParticle = { x: null, y: null, vx: 0, vy: 0, isMouse: true };
-  const mouseConnectionRadius = 200;
+	let width, height;
+	const particles = [];
+	const maxParticles = 350;
+	const mouseParticle = { x: null, y: null, vx: 0, vy: 0, isMouse: true };
+	const mouseConnectionRadius = 200;
 
-  // Redimensiona o canvas conforme a janela
-  function resize() {
-    width = canvas.width = window.innerWidth;
-    height = canvas.height = window.innerHeight;
-  }
+	// Redimensiona o canvas conforme a janela
+	function resize() {
+		width = canvas.width = window.innerWidth;
+		height = canvas.height = window.innerHeight;
+	}
 
-  // Inicializa as partículas com posições e velocidades aleatórias
-  function initParticles() {
-    particles.length = 0;
-    for (let i = 0; i < maxParticles; i++) {
-      particles.push({
-        x: Math.random() * width,
-        y: Math.random() * height,
-        vx: (Math.random() - 0.5) * 1,
-        vy: (Math.random() - 0.5) * 1,
-      });
-    }
-  }
+	// Inicializa as partículas com posições e velocidades aleatórias
+	function initParticles() {
+		particles.length = 0;
+		for (let i = 0; i < maxParticles; i++) {
+			particles.push({
+				x: Math.random() * width,
+				y: Math.random() * height,
+				vx: (Math.random() - 0.5) * 1,
+				vy: (Math.random() - 0.5) * 1,
+			});
+		}
+	}
 
-  // Captura a posição do mouse
-  document.addEventListener('mousemove', (event) => {
-    mouseParticle.x = event.clientX;
-    mouseParticle.y = event.clientY;
-  });
+	// Captura a posição do mouse
+	document.addEventListener('mousemove', (event) => {
+		mouseParticle.x = event.clientX;
+		mouseParticle.y = event.clientY;
+	});
 
-  document.addEventListener('mouseleave', () => {
-    mouseParticle.x = null;
-    mouseParticle.y = null;
-  });
+	document.addEventListener('mouseleave', () => {
+		mouseParticle.x = null;
+		mouseParticle.y = null;
+	});
 
-  // Desenha as partículas e as linhas entre elas
-  function draw() {
-    ctx.clearRect(0, 0, width, height);
-    ctx.fillStyle = 'rgba(254, 200, 112, 0.3)'; // Cor #FEC870 com opacidade reduzida para partículas
-    particles.forEach((particle) => {
-      ctx.beginPath();
-      ctx.arc(particle.x, particle.y, 2, 0, Math.PI * 2);
-      ctx.fill();
-    });
+	// Desenha as partículas e as linhas entre elas
+	function draw() {
+		ctx.clearRect(0, 0, width, height);
+		ctx.fillStyle = 'rgba(254, 200, 112, 0.3)'; // Cor #FEC870 com opacidade reduzida para partículas
+		particles.forEach((particle) => {
+			ctx.beginPath();
+			ctx.arc(particle.x, particle.y, 2, 0, Math.PI * 2);
+			ctx.fill();
+		});
 
-    // Adiciona a partícula do mouse para o desenho das linhas
-    if (mouseParticle.x && mouseParticle.y) {
-      particles.forEach((p) => {
-        const dist = Math.hypot(mouseParticle.x - p.x, mouseParticle.y - p.y);
-        if (dist < mouseConnectionRadius) {
-          ctx.strokeStyle = `rgba(254, 200, 112, ${
-            0.3 * (1 - dist / mouseConnectionRadius)
-          })`; // Cor #FEC870 com opacidade
-          ctx.beginPath();
-          ctx.moveTo(mouseParticle.x, mouseParticle.y);
-          ctx.lineTo(p.x, p.y);
-          ctx.stroke();
-        }
-      });
-    }
+		// Adiciona a partícula do mouse para o desenho das linhas
+		if (mouseParticle.x && mouseParticle.y) {
+			particles.forEach((p) => {
+				const dist = Math.hypot(mouseParticle.x - p.x, mouseParticle.y - p.y);
+				if (dist < mouseConnectionRadius) {
+					ctx.strokeStyle = `rgba(254, 200, 112, ${0.3 * (1 - dist / mouseConnectionRadius)})`; // Cor #FEC870 com opacidade
+					ctx.beginPath();
+					ctx.moveTo(mouseParticle.x, mouseParticle.y);
+					ctx.lineTo(p.x, p.y);
+					ctx.stroke();
+				}
+			});
+		}
 
-    // Conecta as partículas entre si
-    particles.forEach((p1, index) => {
-      for (let i = index + 1; i < particles.length; i++) {
-        const p2 = particles[i];
-        const dist = Math.hypot(p1.x - p2.x, p1.y - p2.y);
-        if (dist < 80) {
-          ctx.strokeStyle = `rgba(254, 200, 112, ${0.2 * (1 - dist / 80)})`; // Cor #FEC870 com opacidade
-          ctx.beginPath();
-          ctx.moveTo(p1.x, p1.y);
-          ctx.lineTo(p2.x, p2.y);
-          ctx.stroke();
-        }
-      }
-    });
-  }
+		// Conecta as partículas entre si
+		particles.forEach((p1, index) => {
+			for (let i = index + 1; i < particles.length; i++) {
+				const p2 = particles[i];
+				const dist = Math.hypot(p1.x - p2.x, p1.y - p2.y);
+				if (dist < 80) {
+					ctx.strokeStyle = `rgba(254, 200, 112, ${0.2 * (1 - dist / 80)})`; // Cor #FEC870 com opacidade
+					ctx.beginPath();
+					ctx.moveTo(p1.x, p1.y);
+					ctx.lineTo(p2.x, p2.y);
+					ctx.stroke();
+				}
+			}
+		});
+	}
 
-  // Atualiza as posições das partículas
-  function update() {
-    particles.forEach((p) => {
-      p.x += p.vx;
-      p.y += p.vy;
+	// Atualiza as posições das partículas
+	function update() {
+		particles.forEach((p) => {
+			p.x += p.vx;
+			p.y += p.vy;
 
-      // Faz as partículas reaparecerem ao atravessar as bordas
-      if (p.x > width) p.x = 0;
-      if (p.x < 0) p.x = width;
-      if (p.y > height) p.y = 0;
-      if (p.y < 0) p.y = height;
-    });
-  }
+			// Faz as partículas reaparecerem ao atravessar as bordas
+			if (p.x > width) p.x = 0;
+			if (p.x < 0) p.x = width;
+			if (p.y > height) p.y = 0;
+			if (p.y < 0) p.y = height;
+		});
+	}
 
-  // Loop de animação
-  function animate() {
-    draw();
-    update();
-    requestAnimationFrame(animate);
-  }
+	// Loop de animação
+	function animate() {
+		draw();
+		update();
+		requestAnimationFrame(animate);
+	}
 
-  // Inicializa o canvas e a animação
-  window.addEventListener('resize', resize);
-  resize();
-  initParticles();
-  animate();
+	// Inicializa o canvas e a animação
+	window.addEventListener('resize', resize);
+	resize();
+	initParticles();
+	animate();
 });
 
 function showContent(contentId) {
-  // Esconde todas as divs de conteúdo
-  document.querySelectorAll('.content').forEach((div) => {
-    div.classList.remove('active');
-  });
+	// Esconde todas as divs de conteúdo
+	document.querySelectorAll('.content').forEach((div) => {
+		div.classList.remove('active');
+	});
 
-  // Mostra apenas a div selecionada
-  document.getElementById(contentId).classList.add('active');
+	// Mostra apenas a div selecionada
+	document.getElementById(contentId).classList.add('active');
 }
+
+// Função para abrir o modal e carregar conteúdo de uma página HTML
+function abrirModal(pagina) {
+	const modal = document.getElementById('modal');
+	const modalBody = document.getElementById('modal-body');
+
+	// Exibir o modal
+	modal.style.display = 'flex';
+
+	// Carregar o conteúdo da página HTML usando fetch
+	fetch(`content/${pagina}`)
+		.then((response) => {
+			if (!response.ok) throw new Error('Erro ao carregar conteúdo');
+			return response.text();
+		})
+		.then((content) => {
+			modalBody.innerHTML = content;
+		})
+		.catch((error) => {
+			modalBody.innerHTML = '<p>Erro ao carregar conteúdo.</p>';
+			console.error(error);
+		});
+}
+
+// Função para fechar o modal
+function fecharModal() {
+	document.getElementById('modal').style.display = 'none';
+	document.getElementById('modal-body').innerHTML = ''; // Limpar conteúdo
+}
+
+// Fechar o modal ao clicar fora da área de conteúdo
+window.onclick = function (event) {
+	const modal = document.getElementById('modal');
+	if (event.target === modal) {
+		fecharModal();
+	}
+};
