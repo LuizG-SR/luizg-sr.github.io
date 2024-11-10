85d824719bdbae867fec82aca5dafbc39eceb773
LuizG-SR <luizgsribeiro@gmail.com>
2024-11-10 10:36:10 -0300

    ..

--- language.js
+++ language.js
@@ -2,30 +2,68 @@ let translations = {};
 
 // Função para carregar as traduções
 async function loadTranslations(lang) {
-	const response = await fetch(`${lang}.json`);
-	translations = await response.json();
-	updateTexts();
+  const response = await fetch(`${lang}.json`);
+  translations = await response.json();
+  updateTexts();
 }
 
 // Função para atualizar o conteúdo dos elementos
 function updateTexts() {
-	document.getElementById('home').innerHTML = translations.home;
-	document.getElementById('curriculum').innerHTML = translations.curriculum;
-	document.getElementById('projects').innerHTML = translations.projects;
-	document.getElementById('contact').innerHTML = translations.contact;
-	document.getElementById('footer').innerHTML = translations.footer;
-	document.getElementById('profile_text').innerHTML = translations.profile_text;
-	document.getElementById('title_proj_1').innerHTML = translations.title_proj_1;
-	document.getElementById('text_proj_1').innerHTML = translations.text_proj_1;
-	document.getElementById('title_proj_2').innerHTML = translations.title_proj_2;
-	document.getElementById('text_proj_2').innerHTML = translations.text_proj_2;
-	document.getElementById('title_proj_3').innerHTML = translations.title_proj_3;
-	document.getElementById('text_proj_3').innerHTML = translations.text_proj_3;
-	document.getElementById('title_proj_4').innerHTML = translations.title_proj_3;
-	document.getElementById('text_proj_4').innerHTML = translations.text_proj_3;
-	document.getElementById('curriculo_title').innerHTML = translations.curriculo_title;
-	document.getElementById('section_header').innerHTML = translations.section_header;
-	document.getElementById('section_resume').innerHTML = translations.section_resume;
+  const elementsToTranslate = {
+    home: 'home',
+    curriculum: 'curriculum',
+    projects: 'projects',
+    contact: 'contact',
+    footer: 'footer',
+    profile_text: 'profile_text',
+    title_proj_1: 'title_proj_1',
+    text_proj_1: 'text_proj_1',
+    title_proj_2: 'title_proj_2',
+    text_proj_2: 'text_proj_2',
+    title_proj_3: 'title_proj_3',
+    text_proj_3: 'text_proj_3',
+    title_proj_4: 'title_proj_4',
+    text_proj_4: 'text_proj_4',
+    curriculo_title: 'curriculo_title',
+    section_header: 'section_header',
+    section_resume: 'section_resume',
+    title_educacao: 'title_educacao',
+    title_carreira: 'title_carreira',
+    cotemig: 'cotemig',
+    faculdade_cotemig: 'faculdade_cotemig',
+    unibh: 'unibh',
+    faculdade_cotemig_ads: 'faculdade_cotemig_ads',
+    xp_educacao: 'xp_educacao',
+    construsite: 'construsite',
+    nix_sistemas: 'nix_sistemas',
+    buscar_id: 'buscar_id',
+    argument: 'argument',
+    dwebnet: 'dwebnet',
+    totvs: 'totvs',
+    titulo_projeto_1: 'titulo_projeto_1',
+    descricao_projeto_1: 'descricao_projeto_1',
+    titulo_projeto_2: 'titulo_projeto_2',
+    descricao_projeto_2: 'descricao_projeto_2',
+    titulo_projeto_3: 'titulo_projeto_3',
+    descricao_projeto_3: 'descricao_projeto_3',
+    titulo_projeto_4: 'titulo_projeto_4',
+    descricao_projeto_4: 'descricao_projeto_4',
+    titulo_projeto_5: 'titulo_projeto_5',
+    descricao_projeto_5: 'descricao_projeto_5',
+  };
+
+  Object.keys(elementsToTranslate).forEach((id) => {
+    const element = document.getElementById(id);
+    if (element && translations[id]) {
+      element.innerHTML = translations[id];
+    }
+
+    // Check if there is a description translation for tooltips
+    const descriptionKey = `${id}_description`;
+    if (element && translations[descriptionKey]) {
+      element.setAttribute('data-description', translations[descriptionKey]);
+    }
+  });
 }
 
 // Carrega o idioma inicial
@@ -33,5 +71,5 @@ loadTranslations('pt');
 
 // Função para trocar o idioma
 function changeLanguage(lang) {
-	loadTranslations(lang);
+  loadTranslations(lang);
 }
